ab8b2a8ffaf6a5dd0b62688af3262d00123f0586
Alexander Sokol <sokol@compatibl.com>
2024-12-15 01:26:52 -0500

    Remove unused __future__ annotations.

--- cl/convince/llms/completion_key.py
+++ cl/convince/llms/completion_key.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
 from dataclasses import dataclass
 from typing import Type
 from typing_extensions import Self
