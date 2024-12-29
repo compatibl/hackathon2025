ccf3cf347d1859c53a1a2113362f2d416c4d7851
Alexander Sokol <sokol@compatibl.com>
2024-12-29 17:51:47 -0500

    Merge field with required and add optional in dataclass extensions.

--- stubs/cl/convince/prompts/stub_prompt_params.py
+++ stubs/cl/convince/prompts/stub_prompt_params.py
@@ -15,7 +15,7 @@
 from dataclasses import dataclass
 from typing import List
 from cl.runtime import RecordMixin
-from cl.runtime.records.dataclasses_extensions import field
+from cl.runtime.records.dataclasses_extensions import required
 from stubs.cl.convince.prompts.stub_prompt_params_key import StubPromptParamsKey
 
 
@@ -29,7 +29,7 @@ class StubPromptParams(StubPromptParamsKey, RecordMixin[StubPromptParamsKey]):
     str_opt: str | None = None
     """Optional string field."""
 
-    str_req_list: List[str] = field(default_factory=lambda: ["str_req_list_1", "str_req_list_2"])
+    str_req_list: List[str] = required(default_factory=lambda: ["str_req_list_1", "str_req_list_2"])
     """Required string list field."""
 
     int_req: int = 123
