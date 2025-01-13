34baaf3b394eab0d240e0e132cfe550c637ba9f6
Alexander Sokol <sokol@compatibl.com>
2025-01-13 07:55:55 -0500

    Invoke init_all on Llm objects created in memory.

--- stubs/cl/convince/experiments/stub_llms.py
+++ stubs/cl/convince/experiments/stub_llms.py
@@ -22,20 +22,20 @@ from cl.convince.llms.llm import Llm
 def get_stub_mini_llms() -> List[Llm]:
     """Mini-LLMs for proof of concept tests."""
     return [
-        FireworksLlamaLlm(llm_id="llama-v3-8b-instruct"),
-        GptLlm(llm_id="gpt-4o-mini"),
+        FireworksLlamaLlm(llm_id="llama-v3-8b-instruct").init_all(),
+        GptLlm(llm_id="gpt-4o-mini").init_all(),
     ]
 
 
 def get_stub_full_llms() -> List[Llm]:
     """Full (but not extravagant) LLMs for prompt design tests."""
     return [
-        ClaudeLlm(llm_id="claude-3-5-sonnet-20240620"),
-        FireworksLlamaLlm(llm_id="llama-v3-70b-instruct"),
-        GptLlm(llm_id="gpt-4o"),
+        ClaudeLlm(llm_id="claude-3-5-sonnet-20240620").init_all(),
+        FireworksLlamaLlm(llm_id="llama-v3-70b-instruct").init_all(),
+        GptLlm(llm_id="gpt-4o").init_all(),
     ]
 
 
 def get_stub_full_llm() -> Llm:
     """Full (but not extravagant) LLM for prompt design tests."""
-    return GptLlm(llm_id="gpt-4o")
+    return GptLlm(llm_id="gpt-4o").init_all()
--- tests/cl/convince/llms/claude/test_claude_llm.py
+++ tests/cl/convince/llms/claude/test_claude_llm.py
@@ -20,7 +20,7 @@ from cl.convince.llms.claude.claude_llm import ClaudeLlm
 def test_smoke(pytest_default_db):
     """Test ClaudeLlm class."""
     llms = [
-        ClaudeLlm(llm_id="claude-3-haiku-20240307"),
+        ClaudeLlm(llm_id="claude-3-haiku-20240307").init_all(),
     ]
     for llm in llms:
         assert "4" in llm.completion("2 times 2?")
