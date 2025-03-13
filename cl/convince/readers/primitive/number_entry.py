22132243be1ccfb8f3e80d76d6c1515fdd065336
Alexander Sokol <sokol@compatibl.com>
2025-03-13 02:23:04 -0400

    Run code formatters.

--- cl/convince/readers/primitive/number_entry.py
+++ cl/convince/readers/primitive/number_entry.py
@@ -14,7 +14,6 @@
 
 from dataclasses import dataclass
 from cl.runtime import RecordMixin
-from cl.runtime.records.for_dataclasses.extensions import required
 from cl.convince.readers.primitive.number_entry_key import NumberEntryKey
 
 
