5de2e83e71fa3e2238029215a0964a230f3f71c6
Alexander Sokol <sokol@compatibl.com>
2025-05-27 16:54:13 -0400

    Run code formatters.

--- cl/convince/readers/template_entry_mixin.py
+++ cl/convince/readers/template_entry_mixin.py
@@ -20,9 +20,9 @@ from typing import Generic
 from cl.runtime.contexts.db_context import DbContext
 from cl.runtime.records.key_mixin import KeyMixin
 from cl.runtime.records.protocols import TKey
-from cl.convince.readers.entry_mixin import EntryMixin
 from cl.runtime.serializers.bootstrap_serializers import BootstrapSerializers
 from cl.runtime.serializers.yaml_encoders import YamlEncoders
+from cl.convince.readers.entry_mixin import EntryMixin
 
 
 @dataclass(slots=True, kw_only=True)
@@ -55,8 +55,8 @@ class TemplateEntryMixin(Generic[TKey], EntryMixin[TKey], ABC):
                 linejunk=lambda x: False,
                 charjunk=lambda x: False,
             )
-            filtered_diff_lines = [line for line in diff_lines if not line.startswith('?')]
-            diff_str = '\n'.join(filtered_diff_lines)
+            filtered_diff_lines = [line for line in diff_lines if not line.startswith("?")]
+            diff_str = "\n".join(filtered_diff_lines)
 
             # Remove fields includes elsewhere
             params_dict = BootstrapSerializers.FOR_UI.serialize(self)
@@ -66,7 +66,9 @@ class TemplateEntryMixin(Generic[TKey], EntryMixin[TKey], ABC):
 
             # Return correction string
             template_body = self.template.body  # noqa
-            return (f"The template and data from your previous response did not match the input text.\n"
-                    f"Please provide a new response that corrects the differences described below.\n\n"
-                    f"Template body:\n\n{template_body}\n\nTemplate parameters:\n\n{params_yaml}\n\n"
-                    f"Unified diff (---InputText +++RenderedText):\n\n{diff_str}\n\n")
+            return (
+                f"The template and data from your previous response did not match the input text.\n"
+                f"Please provide a new response that corrects the differences described below.\n\n"
+                f"Template body:\n\n{template_body}\n\nTemplate parameters:\n\n{params_yaml}\n\n"
+                f"Unified diff (---InputText +++RenderedText):\n\n{diff_str}\n\n"
+            )
