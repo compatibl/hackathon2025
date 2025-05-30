20a86fa2e4cbd9016e05384f737a4ca3fb291fc3
Alexander Sokol <sokol@compatibl.com>
2025-05-30 02:25:29 -0400

    Return frozendict instead of tuple from get_generic_args.

--- cl/convince/readers/entry_reader_mixin.py
+++ cl/convince/readers/entry_reader_mixin.py
@@ -36,7 +36,7 @@ class EntryReaderMixin(Generic[TKey, TEntry], RecordMixin[TKey], ABC):
     def get_entry_type(cls):
         """The actual type passed as TEntry argument to the generic definition of this class or its descendants."""
         # Second argument of EntryReaderMixin[TKey, TEntry]
-        return GenericUtil.get_generic_args(cls, EntryReaderMixin)[1]
+        return GenericUtil.get_generic_args(cls, EntryReaderMixin)["TEntry"]
 
     @abstractmethod
     def read(self, text: str) -> TEntry:
