5dde3c0106972b02f36721eb6303bdacd67f7f46
Alexander Sokol <sokol@compatibl.com>
2025-08-15 15:12:27 -0400

    Update imports after the directory change.

--- cl/convince/retrievers/retrieval_query.py
+++ cl/convince/retrievers/retrieval_query.py
@@ -15,7 +15,7 @@
 from dataclasses import dataclass
 from cl.runtime.records.for_dataclasses.extensions import required
 from cl.runtime.records.key_mixin import KeyMixin
-from cl.runtime.records.query_mixin import QueryMixin
+from cl.runtime.db.query_mixin import QueryMixin
 from cl.convince.retrievers.annotating_retrieval import AnnotatingRetrieval
 from cl.convince.retrievers.retriever_key import RetrieverKey
 
